a57c205c8573b63f9c0d67d8729604413752ba1c
jackdbd <jackdebidda@gmail.com>
2017-11-25 23:55:42 +0100

    Add snippets

---
+++ snippets/create_table.py
@@ -0,0 +1,37 @@
+import tables as tb
+
+
+class Particle(tb.IsDescription):
+    # character String
+    identity = tb.StringCol(itemsize=22, dflt=' ', pos=0)
+    # short integer
+    idnumber = tb.Int16Col(dflt=1, pos=1)
+    # single-precision
+    speed = tb.Float32Col(dflt=1, pos=2)
+
+
+def fill_table(table, num_records):
+    # Get the record object associated with the table:
+    row = table.row
+    for i in range(num_records):
+        row['identity'] = 'This is particle: {}'.format(i)
+        row['idnumber'] = i
+        row['speed'] = i * 2.0
+        # inject the Record values into the table
+        row.append()
+    # Flush the table buffers
+    table.flush()
+
+
+def main():
+    with tb.open_file('pytables-tables.h5', 'w') as hdf:
+        group0 = hdf.create_group(where=hdf.root, name='group0')
+        group1 = hdf.create_group(where='/', name='group1')
+        table0 = hdf.create_table(group0, 'table0', description=Particle)
+        table1 = hdf.create_table('/group1', 'table1', description=Particle)
+
+        for table in (table0, table1):
+            fill_table(table, 10)
+
+if __name__ == '__main__':
+    main()
---
+++ snippets/create_table_nested.py
@@ -0,0 +1,58 @@
+import numpy as np
+import tables as tb
+
+
+class Particle(tb.IsDescription):
+    # 16-character String
+    name = tb.StringCol(16)
+    # signed 64-bit integer
+    idnumber = tb.Int64Col()
+    # unsigned short integer
+    ADCcount = tb.UInt16Col()
+    # unsigned byte
+    TDCcount = tb.UInt8Col()
+    # integer
+    grid_i = tb.Int32Col()
+    # integer
+    grid_j = tb.Int32Col()
+
+    # A sub-structure (nested data-type)
+    class Properties(tb.IsDescription):
+        # 2-D float array (single-precision)
+        pressure = tb.Float32Col(shape=(2, 3))
+        # 3-D float array (double-precision)
+        energy = tb.Float64Col(shape=(2, 3, 4))
+
+
+def fill_table(table, num_records):
+    row = table.row
+    for i in range(num_records):
+        # First, assign the values to the Particle record
+        row['name'] = np.random.choice(['Superman', 'Spider-Man', 'Batman'])
+        row['idnumber'] = np.random.randint(0, 100, dtype='int64')
+        row['ADCcount'] = i * 2.0
+        # TDCcount is an unsigned byte. Negative numbers or numbers greater
+        # than 255 (2^8 - 1) will be saved, but you will have invalid data in
+        # the table (-10 will appear as 246)
+        # row['TDCcount'] = -10
+        row['TDCcount'] = np.random.randint(0, 255,
+                                            Particle.columns['TDCcount'].dtype)
+        row['grid_i'] = np.random.randint(0, 100, dtype='int32')
+        row['grid_j'] = np.random.randint(0, 100, dtype='int32')
+
+        # Fill the nested data-type Properties
+        row['Properties/pressure'] = np.random.random((2, 3))
+        row['Properties/energy'] = np.random.random((2, 3, 4))
+
+        row.append()
+
+    table.flush()
+
+
+def main():
+    with tb.open_file('pytables-tables-nested.h5', 'w') as hdf:
+        my_table = hdf.create_table('/', 'my_table', description=Particle)
+        fill_table(my_table, 100)
+
+if __name__ == '__main__':
+    main()
