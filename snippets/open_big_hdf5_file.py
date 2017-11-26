624118e62c24790b2921c6d468a9b5ffe67a6550
jackdbd <jackdebidda@gmail.com>
2017-11-26 12:44:48 +0100

    Update snippet to read big hdf5 file

--- snippets/open_big_hdf5_file.py
+++ snippets/open_big_hdf5_file.py
@@ -1,7 +1,7 @@
 import time
 import os
 import tables as tb
-import numpy as np
+from hurry.filesize import size
 
 
 def main():
@@ -12,20 +12,52 @@ def main():
     with tb.open_file(file_path, 'r') as hdf:
         table = hdf.root.Day_001.control
         print(table)
+        print('Table size in memory: {}'.format(size(table.size_in_memory)))
+        print('Table size on disk: {}'.format(size(table.size_on_disk)))
+        print('Row size: {}'.format(size(table.rowsize)))
 
-        # t0 = time.time()
-        # np.where(arr[:] > 0.5)
-        # we can also use .read()
-        # np.where(arr.read() > 0.5)
-        # t1 = time.time()
-        # print('np.where took {:.2f}ms'.format((t1 - t0) * 1000))
+        # querying with numpy is possible only it the table fits in memory
+        t0 = time.time()
+        results = [x['heart_rate'] for x in table.read()
+                   if 60 < x['heart_rate'] < 70 and 40 < x['hematocrit'] < 60]
+        t1 = time.time()
+        print('Querying with numpy took {:.2f}ms'.format((t1 - t0) * 1000))
 
-        # t0 = time.time()
-        # tb.where
-        # aa = np.where(arr[:] > 0.5)
-        # t1 = time.time()
-        # print('np.where took {:.2f}ms'.format((t1 - t0) * 1000))
+        # table.iterrows() returns an iterator that iterates over all rows.
+        # This allow us to avoid loading the entire table in memory.
+        t0 = time.time()
+        results = [x['heart_rate'] for x in table.iterrows()
+                   if 60 < x['heart_rate'] < 70 and 40 < x['hematocrit'] < 60]
+        t1 = time.time()
+        print('Querying with table.iterrows took {:.2f}ms'
+              .format((t1 - t0) * 1000))
 
+        t0 = time.time()
+        results = [x['heart_rate'] for x in table.where(
+            """((60 < heart_rate) & (heart_rate < 70)) & ((40 < hematocrit) & (hematocrit < 60))""")]
+        t1 = time.time()
+        print('Querying with table.where took {:.2f}ms'
+              .format((t1 - t0) * 1000))
+
+        t0 = time.time()
+        results = [x['heart_rate'] for x in table.read_where(
+            """((60 < heart_rate) & (heart_rate < 70)) & ((40 < hematocrit) & (hematocrit < 60))""")]
+        t1 = time.time()
+        print('Querying with table.read_where took {:.2f}ms'
+              .format((t1 - t0) * 1000))
+
+        # this won't work: you can't use Python's standard boolean operators in
+        # NumExpr expressions
+        condition = """(60 < heart_rate < 70) & (40 < hematocrit < 60)"""
+
+        # this one works
+        condition = """((60 < heart_rate) & (heart_rate < 70)) & ((40 < hematocrit) & (hematocrit < 60))"""
+        # we can make it more readable
+        cond0 = '((60 < heart_rate) & (heart_rate < 70))'
+        cond1 = '((40 < hematocrit) & (hematocrit < 60))'
+        condition = '{} & {}'.format(cond0, cond1)
+        results = [x['heart_rate'] for x in table.read_where(condition)]
+        print(len(results))
 
 if __name__ == '__main__':
     main()
