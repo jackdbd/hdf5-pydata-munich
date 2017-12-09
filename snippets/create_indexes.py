c43ca993846ee1e1ca6e51a788801b4dd98abbfe
jackdbd <jackdebidda@gmail.com>
2017-12-09 09:32:08 +0100

    Update snippet that appends indexes to a table

--- snippets/create_indexes.py
+++ snippets/create_indexes.py
@@ -1,29 +1,62 @@
 """Create indexes in a table.
 
-Run the snippet 'open_big_hdf5.py' first, and take note of the timings.
-The run this snippet, which creates indexes for the columns used when querying
-the data in 'open_big_hdf5.py'.
-Run the snippet 'open_big_hdf5.py' once again. Querying with numpy or .iterrows
-should take the same amount of time. Querying with .where or .read_where should
-be faster.
+Once the indexes have been created, querying with table.read or table.iterrows
+should take the same amount of time, while querying with table.where or
+table.read_where should be faster.
+
+See Also
+--------
+- create_csindex: Create a completely sorted index (CSI) 
+- Search with index might be slower than without index.
+  This happens when the number of results is big.
+  https://stackoverflow.com/questions/20769818/search-with-index-is-slower-than-without-index-in-pytables-when-the-result-is-la
 """
 import os
+import time
 import tables as tb
 
 
 def main():
     here = os.path.abspath(os.path.dirname(__file__))
     data_dir = os.path.abspath(os.path.join(here, '..', 'data'))
-    file_path = os.path.join(data_dir, 'big_file.h5')
+    h5_file_path = os.path.join(data_dir, 'NYC-yellow-taxis-indexed.h5')
 
-    # open file in append mode
-    with tb.open_file(file_path, 'a') as hdf:
-        table = hdf.root.Day_001.control
+    table_where = '/yellow_{year}_{month}'.format(year='2015', month='01')
 
-        # Create indexes
-        blosc_filter = tb.Filters(complevel=9, complib='blosc')
-        table.cols.heart_rate.create_csindex(filters=blosc_filter)
-        table.cols.hematocrit.create_csindex(filters=blosc_filter)
+    # Indexes support compression!
+    filters = tb.Filters(complevel=5, complib='blosc')
+
+    t0 = time.time()
+    # Indexes create a group in the HDF5 file, so we must open in 'a'ppend mode
+    with tb.open_file(filename=h5_file_path, mode='a') as f:
+        table = f.get_node(where=table_where)
+        
+        try:
+            table.cols.total_amount.create_index(filters=filters)
+        except ValueError as e:
+            print(e)
+            table.cols.total_amount.remove_index()
+            print('Index removed. Create new index')
+            table.cols.total_amount.create_index(filters=filters)
+
+        try:
+            table.cols.passenger_count.create_index(filters=filters)
+        except ValueError as e:
+            print(e)
+            table.cols.passenger_count.remove_index()
+            print('Index removed. Create new index')
+            table.cols.passenger_count.create_index(filters=filters)
+
+        try:
+            table.cols.trip_distance.create_index(filters=filters)
+        except ValueError as e:
+            print(e)
+            table.cols.trip_distance.remove_index()
+            print('Index removed. Create new index')
+            table.cols.trip_distance.create_index(filters=filters)
+
+    t1 = time.time()
+    print('Creating indexes took {:.2f}s'.format(t1 - t0))
 
 if __name__ == '__main__':
     main()
